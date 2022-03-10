25d43fe1934512701716c500d9b6cfcc22f4b5c2
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-03-10 18:01:45 +0100

    Add working price value

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
       console.log('new Product: ', thisProduct);
     }
     renderInMenu() {
@@ -100,12 +102,57 @@
         thisProduct.element.classList.toggle('active');
       });
     }
+    initOrderForm() {
+      const thisProduct = this;
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+    processOrder() {
+      const thisProduct = this;
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData ' + Object.values(formData));
+      let price = thisProduct.data.price;
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log('++' + paramId, param);
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          if(formData[paramId].includes(optionId) && option[Object.keys(option)[2]]!=true) {
+            price += option[Object.keys(option)[1]];
+            console.log(price);
+          }
+          else if(!formData[paramId].includes(optionId) && option[Object.keys(option)[2]]==true) {
+            price -= option[Object.keys(option)[1]];
+          }
+          console.log('+' + optionId, option);
+        }
+
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+    }
   }
 
   const app = {
     initMenu: function(){
       const thisApp = this;
-
       for(let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
