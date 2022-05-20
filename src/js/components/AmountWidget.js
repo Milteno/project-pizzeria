85900f9ade496fbc4cd22a47a38bfbed3d979c70
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-05-20 13:52:57 +0200

    Cannot use import

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,54 +1,45 @@
 import {select, settings} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-
-class AmountWidget {
+class AmountWidget extends BaseWidget{
   constructor(element) {
+    super(element, settings.amountWidget.defaultValue);
     const thisWidget = this;
     thisWidget.getElements(element);
-    thisWidget.setValue(thisWidget.input.value);
     thisWidget.initActions(event);
   }
-  getElements(element) {
+  getElements() {
     const thisWidget = this;
 
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
-    thisWidget.value = settings.amountWidget.defaultValue;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
-  setValue(value) {
+
+  isValid(value) {
+    return !isNaN(value) && value >= settings.amountWidget.defaultMin && value <= settings.amountWidget.defaultMax;
+  }
+
+  renderValue() {
     const thisWidget = this;
-
-    const newValue = parseInt(value);
-
-    if (newValue !== thisWidget.value && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) {
-      thisWidget.value = newValue;
-    }
-    thisWidget.input.value = thisWidget.value;
-    thisWidget.announce();
+    thisWidget.dom.input.value = thisWidget.value;
   }
+
+
   initActions() {
     const thisWidget = this;
-    thisWidget.input.addEventListener('change', function () {
+    thisWidget.dom.input.addEventListener('change', function () {
 
-      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.setValue(thisWidget.dom.input.value);
     });
-    thisWidget.linkDecrease.addEventListener('click', function (event) {
+    thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
     });
-    thisWidget.linkIncrease.addEventListener('click', function(event) {
+    thisWidget.dom.linkIncrease.addEventListener('click', function(event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
-  }
-  announce() {
-    const thisWidget = this;
-    const event = new CustomEvent('updated', {
-      bubbles: true
-    });
-    thisWidget.element.dispatchEvent(event);
   }
 }
 export default AmountWidget;
