7883b3e3f92be6884112ec497c9d77d4c806731e
Grzegorz Przezdziecki <mlgforest6654@gmail.com>
2022-06-30 16:47:37 +0200

    ESLint wciecia, odstepy

--- src/js/settings.js
+++ src/js/settings.js
@@ -119,7 +119,7 @@ export const settings = {
     tableIdAttribute: 'data-table',
   },
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
     orders: 'orders',
     bookings: 'bookings',
